8310083bcc04946746555e9218861a437473a068
plondon <plondon@gm.slc.edu>
2013-12-15 18:50:30 -0500

    Add precompiled assets for Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -11,11 +11,11 @@
 # if you're sharing your code publicly.
 require 'securerandom'
 def secure_token
-	token_file = Rails.root.joing('.secret')
+	token_file = Rails.root.join('.secret')
 	if File.exist?(token_file)
 		File.read(token_file).chomp
 	else
-		token = SecureRandom.hext(64)
+		token = SecureRandom.hex(64)
 		File.write(token_file, token)
 		tokenend
 	end
