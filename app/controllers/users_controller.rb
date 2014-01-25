fff2b68f24f416f3b83c23d2ef40050464634a49
plondon <plondon@gm.slc.edu>
2014-01-25 15:54:20 -0500

    add updates and authorizations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -7,6 +10,18 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   def create
   	@user = User.new(user_params)
   	if @user.save
@@ -23,4 +38,16 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    def signed_in_user
+      unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  end
 end
